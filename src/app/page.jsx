bad7a159db501ab78a69df56fa8b11073efbad17
rayruenheck <rayruenheck@gmail.com>
2024-03-18 18:14:45 -0400

    minor adjustments

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     
     <div>
-      <a href="/ingredients">ingredients<br/></a>
-      <a href="/searchrecipe">search recipe<br/></a>
+      <a href="/pantry">pantry<br/></a>
+      <a href="/recipes">recipes<br/></a>
       <a href="/login">login<br/></a>
       <a href="/register">register<br/></a>
       
