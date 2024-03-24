fcfa0b49680cfb538958779a6935279ba527ffcf
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2024-03-24 16:55:18 -0500

    in progress

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     
     <div>
-      <a href="/ingredients">ingredients<br/></a>
+      <a href="/pantry">ingredients<br/></a>
       <a href="/searchrecipe">search recipe<br/></a>
       <a href="/login">login<br/></a>
       <a href="/register">register<br/></a>
